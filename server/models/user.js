a537b810abaaa3cfd5cd59f0c3ac75e6f6dbf954
Nate Taylor <taylonr@gmail.com>
2018-05-15 21:19:47 -0500

    Adding date to user model

--- server/models/user.js
+++ server/models/user.js
@@ -7,7 +7,8 @@ module.exports = (sequelize, DataTypes) => {
     },
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
-    wishlistId: DataTypes.INTEGER
+    wishlistId: DataTypes.INTEGER,
+    customerSince: DataTypes.DATE
   });
 
   user.associate = (models) => {
